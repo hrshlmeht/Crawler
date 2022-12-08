9ede0be205b000d7a473d6880271116928b90cb3
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-08 01:16:49 -0500

    12th video

--- spider.py
+++ spider.py
@@ -36,10 +36,26 @@ class Spider:
         if page_url not in Spider.crawled:
             print(thread_name + 'crawling' + page_url)
             print('Queue' + str(len(Spider.queue)) + '| Crawled ' + str(len(Spider.crawled)))
-            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.add_links_to_queue(Spider.gather_links(page_url))
             Spider.queue.remove(page_url)
             Spider.crawled.add(page_url)
             Spider.update_files()
+
+    @staticmethod
+    def gather_links(page_url):
+        html_string = ''
+        try:
+            response = urlopen(page_url)
+            if response.getheader('Content-Type') == 'text/html':
+                html_bytes = response.read()
+                html_string = html_bytes.decode("utf-8")
+            finder = LinkFinder(Spider.base_url , page_url)
+            finder.feed(html_string)
+        except:
+            print('Error: cannot crawl the page')
+            return set()
+        return finder.page_links()
+
 
 
 
