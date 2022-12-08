ece9688f689628f672f6670a2a7c7ce670839254
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-08 02:56:36 -0500

    ab apun analytics karega

--- main.py
+++ main.py
@@ -11,11 +11,26 @@ QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
 NUMBER_OF_THREADS = 8
 queue = Queue()
-Spider(PROJECT_NAME , HOMEPAGE , DOMAIN_NAME)
+Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
 
+# create worker threads(will die when the main exits)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
 
-#each queued links is a new job
+
+# for doing the next job in the queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
+
+# each queued links is a new job
 def create_jobs():
     for link in file_to_sets(QUEUE_FILE):
         queue.put(link)
@@ -27,12 +42,9 @@ def create_jobs():
 def crawl():
     queue_links = file_to_sets(QUEUE_FILE)
     if len(queue_links) > 0:
-        print(str(len(queue_links)) +  'links in the queue')
+        print(str(len(queue_links)) + 'links in the queue')
         create_jobs()
 
 
-
-
-
-
-
+create_workers()
+crawl()
