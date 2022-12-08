2cdd13814efbd8bd82562df14c883e78abf03a86
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-08 01:37:34 -0500

    14th video

---
+++ domain.py
@@ -0,0 +1,20 @@
+from urllib.parse import urlparse
+
+
+#get domain name
+
+def get_domain_name(url):
+    try:
+        results = get_sub_domain_name(url).split('.')
+        return results[-2]+ '.'+ results[-1]
+    except:
+        return ''
+
+#get Sub domain (mail.name.example.com)
+def get_sub_domain_name(url):
+    try:
+        return urlparse(url).netloc
+    except:
+        return ''
+
+
