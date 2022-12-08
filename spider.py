e18e5ddc62416ab9bf976c9a3f43bc7011a7bd5c
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-07 22:08:52 -0500

    9th video

--- spider.py
+++ spider.py
@@ -15,7 +15,18 @@ class Spider:
     queue = set()
     crawled = set()
 
-    def __init__(self):
+    def __init__(self , project_name , base_url , domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + './queue.txt'
+        Spider.crawled_file = Spider.project_name + './crawled.txt'
+        self.boot()
+        self.crawl_page('First Spider' , Spider.base_url)
+
+
+
+
 
 
 
