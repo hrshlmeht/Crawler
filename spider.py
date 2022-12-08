acd3d5212ba98ade2f7de1f385006e9b4092b351
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-08 01:05:52 -0500

    11th video

--- spider.py
+++ spider.py
@@ -31,6 +31,20 @@ class Spider:
         Spider.queue = file_to_sets(Spider.queue_file)
         Spider.crawled = file_to_sets(Spider.crawled_file)
 
+    @staticmethod
+    def crawl_page(thread_name , page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + 'crawling' + page_url)
+            print('Queue' + str(len(Spider.queue)) + '| Crawled ' + str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
+
+
+
+
+
 
 
 
